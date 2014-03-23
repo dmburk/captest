bc241675dcba826679857533129c5482189900a2
Michael Burk <michael.burk@gmail.com>
2014-03-23 18:21:42 -0400

    Edited capistrano configs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'captest'
+set :repo_url, 'git@github.com:dmburk/captest.git'
+set :deploy_to, "/home/rails/captest"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
