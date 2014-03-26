9139372887e5f958f8ee73c9b5cabc12c4dd3db3
Michael Burk <michael.burk@gmail.com>
2014-03-26 19:49:44 -0400

    Generated a test controller to test deployments with.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Captest::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'test#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
